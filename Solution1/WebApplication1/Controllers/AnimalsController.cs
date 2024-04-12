f47d1b2f779d8bebe206bf813d6687825aefdf33
Mateusz Wdzienkowski <mateuszwdzienkowski@gmail.com>
2024-04-12 13:38:03 +0200

    Added connection with DB

---
+++ Solution1/WebApplication1/Controllers/AnimalsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplication1.Controllers;
+
+[Route("/api/animals")]
+[ApiController]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        using SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM Animal"; //query 
+        
+        
+        
+        return Ok();
+    }
+    
+    
+}
